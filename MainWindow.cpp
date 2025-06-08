e1b4b32d368264fc1eb66913b5e314630baff1b6
CharNoe <24380182+CharNoe@users.noreply.github.com>
2025-06-08 11:14:30 +0900

    init

---
+++ MainWindow.cpp
@@ -0,0 +1,18 @@
+#include "MainWindow.hpp"
+#include "ui_MainWindow.h"
+
+namespace gui {
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+} // namespace gui
---
+++ MainWindow.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <QMainWindow>
+
+namespace gui {
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+};
+
+} // namespace gui
